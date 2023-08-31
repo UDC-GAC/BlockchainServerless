65b37c87ec4a0dd5fcf645c27f03575471b0b791
JonatanEnes <jonatan.enes@udc.es>
2023-08-31 11:54:50 +0200

    Fix

--- gridcoin-run.sh
+++ gridcoin-run.sh
@@ -4,4 +4,4 @@ then
       exit 1
 fi
 
-gridcoinresearchd -rpcconnect=192.168.51.100 -rpcport=9090 -rpcuser=gridcoinrpc -rpcpassword=Bt2oEfVgnMGqvB26UapLERmDu5bvULKr9SPvPBkMkMSV $1
+gridcoinresearchd -rpcconnect=192.168.51.100 -rpcport=9090 -rpcuser=gridcoinrpc -rpcpassword=Bt2oEfVgnMGqvB26UapLERmDu5bvULKr9SPvPBkMkMSV "$@"
