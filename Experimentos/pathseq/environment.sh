b6ac7f54e95ba2c28ec48eeff15ba551a92f5370
JonatanEnes <jonatan.enes@udc.es>
2023-10-24 19:59:22 +0200

    Se borran ficheros de experimentacion, se gestionan por separado Se modifica la info de los candidatos a experimentos despues de hacer mas pruebas

--- Experimentos/pathseq/environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export InputDir="/root/data/input"
-export OutputDir="/root/data/output"
-export GatkDir="/root/gatk/gatk"
-export ScriptsDir="/opt/plane/pathseq/scripts"
-export PAR_DEGREE=10
