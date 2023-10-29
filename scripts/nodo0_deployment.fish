869bd6f3d3d018a739e6ea9acd3f96fbfcb4f00d
JonatanEnes <jonatan.enes@udc.es>
2023-10-29 17:32:54 +0100

    WIP

--- scripts/nodo0_deployment.fish
+++ scripts/nodo0_deployment.fish
@@ -1,3 +1,5 @@
+#!/usr/bin/fish
+
 # Estos son los comandos a ejecutar en el 'nodo 0', el nodo que se use para
 # ejecutar las cargas experimentales propiamente
 
@@ -10,12 +12,23 @@ git clone https://github.com/UDC-GAC/BDWatchdog.git
 git clone -b blockchain-experiments https://github.com/UDC-GAC/ServerlessContainers
 sed -i 's/lxc/apptainer/g' ServerlessContainers/services_config.yml
 
-
 # Construir los contenedores
 apptainer build --force base.sif base.def
 
-
+# Arrancar el contenedor de experimentacion
+set -gx CONT_NAME "cont0"
+sudo apptainer instance start --hostname {$CONT_NAME} base.sif {$CONT_NAME}
 
 # Descargar el script de Oscar para cambiar permisos
 wget https://raw.githubusercontent.com/UDC-GAC/ServerlessYARN/master/ansible/provisioning/scripts/change_cgroupsv1_permissions.py
-python3 change_cgroupsv1_permissions.py apptainer singularity base
+python3 change_cgroupsv1_permissions.py apptainer singularity {$CONT_NAME}
+
+# Arrancar el Node Scaler
+source ServerlessContainers/set_pythonpath.fish
+tmux new -s "NODE_SCALER" "python3 ServerlessContainers/src/NodeRescaler/NodeRescaler.py"
+
+
+# Arrancar el MetricsFeeder en el contenedor de experimentos
+
+
+
--- scripts/nodo1_deployment.fish
+++ scripts/nodo1_deployment.fish
@@ -1,3 +1,4 @@
+#!/usr/bin/fish
 
 # Estos son los comandos a ejecutar en el 'nodo 1', el nodo auxiliar
 # usado para levantar el framework de Serverless Containers y cualquier 
@@ -10,30 +11,31 @@ apptainer build --force sc.sif sc.def
 # Levantar la StateDatabase (Couchdb)
 set -gx COUCHDB_DATA "/scratch2/couchdb-data"
 rm -Rf {$COUCHDB_DATA}
-mkdir /home/jonatan.enes/couchdb-data
-apptainer instance start --bind /home/jonatan.enes/couchdb-data:/opt/couchdb/data --hostname couchdb couchdb.sif couchdb
+mkdir {$COUCHDB_DATA}
+apptainer instance start --bind {$COUCHDB_DATA}:/opt/couchdb/data --hostname couchdb couchdb.sif couchdb
+tmux new -s "Couchdb" "apptainer exec instance://couchdb /opt/couchdb/bin/couchdb"
 
 # Crear el fichero 'myhosts'
 cat /etc/hosts > myhosts
-echo "dante.dec.udc.es opentsdb" >> myhosts
+echo "193.144.50.38 opentsdb" >> myhosts
 echo "127.0.0.1 couchdb" >> myhosts
 echo "127.0.0.1 orchestrator" >> myhosts
-echo "compute-2-1 host0" >> myhosts
+echo "10.10.255.232 host0" >> myhosts
 
 
 # Levantar el contenedor de SC
 apptainer instance start --hostname sc --bind /home/jonatan.enes/myhosts:/etc/hosts sc.sif sc
+
+# Arrancar el Orquestador y configurar el entorno
 tmux new -s "Orchestrator" "apptainer exec instance://sc bash ServerlessContainers/scripts/services/orchestrator/start.sh"
-
-apptainer shell instance://sc
-
 apptainer exec instance://sc bash ServerlessContainers/conf/create_basics.sh
 apptainer exec instance://sc bash ServerlessContainers/conf/subscribe_all.sh
 
-~~~~~
-fish
-cd ServerlessContainers
-bash conf/create_basics.sh
 
-~~~
+# Arrancar los otros servicios
+tmux new -s "Guardian" "apptainer exec instance://sc bash ServerlessContainers/scripts/services/guardian/start.sh"
+tmux new -s "Scaler" "apptainer exec instance://sc bash ServerlessContainers/scripts/services/scaler/start.sh"
+tmux new -s "DatabaseSnapshoter" "apptainer exec instance://sc bash ServerlessContainers/scripts/services/database_snapshoter/start.sh"
+tmux new -s "StructuresSnapshoter" "apptainer exec instance://sc bash ServerlessContainers/scripts/services/structure_snapshoter/start.sh"
+
 
