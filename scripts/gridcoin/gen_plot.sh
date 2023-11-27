5fd1a5531c556f186cfa84bcad2aea0d1dd4a000
JonatanEnes <jonatan.enes@udc.es>
2023-11-27 20:03:59 +0100

    Added scripts for the grc transactions experiments, and to plot it

---
+++ scripts/gridcoin/gen_plot.sh
@@ -0,0 +1,5 @@
+source /home/jonatan/Desktop/development/ServerlessContainersReportGenerator/set_pythonpath.sh
+
+python3 grc_exp.py
+
+cp grc_transactions.png /home/jonatan/Desktop/development/ServerlessContainersReportGenerator/REPORTS/
