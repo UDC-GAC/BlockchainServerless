b227f4ee76f4fd0bb60ebf878b919514be877354
JonatanEnes <jonatan.enes@udc.es>
2023-12-09 20:21:49 +0100

    Config for the last experiment as used

--- scripts/experiments/genomics_4fold.sh
+++ scripts/experiments/genomics_4fold.sh
@@ -15,6 +15,21 @@ export MAX_DEBT="-3"
 export POLICY="conservative"
 export TIMEOUT=120
 
+export exp_name="genomics_4fold_1"
+setup_exp
+run_4_fold_exp
+sleep 120
+
+export exp_name="genomics_4fold_2"
+setup_exp
+run_4_fold_exp
+sleep 120
+
+export exp_name="genomics_4fold_3"
+setup_exp
+run_4_fold_exp
+sleep 120
+
 export exp_name="genomics_4fold_4"
 setup_exp
 run_4_fold_exp
