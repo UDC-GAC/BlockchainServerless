4dce155447b7468f50cd5952a6ff301196713373
JonatanEnes <jonatan.enes@udc.es>
2026-03-25 16:41:06 +0100

    Repository reorganized

---
+++ microservices/CreditManager.py
@@ -0,0 +1,359 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Universidade da Coruña
+# Authors:
+#     - Jonatan Enes [main](jonatan.enes@udc.es)
+#     - Roberto R. Expósito
+#     - Juan Touriño
+#
+# This file is part of the ServerlessContainers framework, from
+# now on referred to as ServerlessContainers.
+#
+# ServerlessContainers is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# ServerlessContainers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ServerlessContainers. If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import print_function
+
+import json
+from threading import Thread
+import time
+import traceback
+import logging
+import subprocess
+
+from src.Guardian.Guardian import Guardian
+from src.MyUtils.MyUtils import MyConfig, log_error, get_service, beat, log_info, log_warning, \
+    wait_operation_thread, end_epoch, start_epoch
+import src.StateDatabase.couchdb as couchdb
+import src.StateDatabase.opentsdb as bdwatchdog
+
+CONFIG_DEFAULT_VALUES = {"POLLING_FREQUENCY": 10,
+                         "THRESHOLD": 0.1,
+                         "MIN_COIN_MOVEMENT": 0.05,
+                         "ACTIVE": True,
+                         "GRIDCOIN_RPC_USER": "gridcoinrpc",
+                         "GRIDCOIN_RPC_IP": "192.168.51.100",
+                         "GRIDCOIN_RPC_PORT": "9090",
+                         "GRIDCOIN_RPC_PASS": "Bt2oEfVgnMGqvB26UapLERmDu5bvULKr9SPvPBkMkMSV",
+                         "COINS_TO_CREDIT_RATIO": 600,  # 60 credits per 1 GRC -> 1 vcore for 10 minutes
+                         "DEBUG": True}
+
+SERVICE_NAME = "credit_manager"
+
+
+class CreditManager:
+
+    def __init__(self):
+        self.couchdb_handler = couchdb.CouchDBServer()
+        self.opentsdb_handler = bdwatchdog.OpenTSDBServer()
+        self.debug = True
+        self.config = {}
+
+    def config_grc_connection(self):
+        self.base_call = ["gridcoinresearchd",
+                          "-rpcconnect={0}".format(self.grc_ip),
+                          "-rpcport={0}".format(self.grc_port),
+                          "-rpcuser={0}".format(self.grc_user),
+                          "-rpcpassword={0}".format(self.grc_pass)]
+
+    def get_users_wallets(self):
+        result = self.run_grc_command(["listaccounts"])
+        return json.loads(result)
+
+    def run_grc_command(self, command):
+        return subprocess.run(self.base_call + command, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    def test_grc_connection(self):
+        result = self.run_grc_command(["listaccounts"])
+        try:
+            json.loads(result)
+            return True
+        except json.decoder.JSONDecodeError:
+            log_error("The response from the RPC command did not return a JSON as expected, check the configuration",
+                      self.debug)
+            return False
+
+    def move_credit(self, first_user, second_user, amount):
+        result = self.run_grc_command(["move", first_user, second_user, amount])
+        return result
+
+    def rebase_counters(self, user):
+        REBASE_RATIO = self.min_coin_movement
+        REBASE_BLOCK = int(self.min_coin_movement * self.coins_credit_ratio)
+        coins_per_fold = REBASE_RATIO * 1
+        accounting = user["accounting"]
+        uname = user["name"]
+
+        num_folds = int(accounting["pending"] / REBASE_BLOCK)
+        if num_folds >= 1:
+            coins_moved = num_folds * coins_per_fold
+            success = self.move_credit(uname, "sink", str(coins_moved))
+            if success:
+                accounting["pending"] = accounting["pending"] - num_folds * REBASE_BLOCK
+                accounting["coins"] -= coins_moved
+                accounting["credit"] = self.coins_credit_ratio * accounting["coins"]
+                log_info("User {0} counters have been rebased".format(uname), self.debug)
+            else:
+                log_warning("Could not move credit from user {0} to {1}, rebase not carried out".format(uname, "sink"), self.debug)
+
+    def restrict_container(self, cont, cont_boundary):
+        cont["guard"] = False
+        self.couchdb_handler.update_structure(cont)
+        cont_cpu = cont["resources"]["cpu"]
+        amount = -1 * (cont_cpu["current"] - (cont_cpu["min"] + 2 * cont_boundary))
+        if amount != 0:
+            request = Guardian().generate_request(cont, amount, "cpu")
+            log_info("Sending request to scale an amount of {0} cpu shares".format(amount), self.debug)
+            self.couchdb_handler.add_request(request)
+
+    def check_restriction(self, user):
+        apps = user["applications"]
+        for app_name in apps:
+            app_info = self.couchdb_handler.get_structure(app_name)
+            for cont_name in app_info["containers"]:
+                cont_info = self.couchdb_handler.get_structure(cont_name)
+                cont_limits = self.couchdb_handler.get_limits({"name": cont_name})  # TODO This is pretty shoddy I know
+                cont_cpu_boundary = cont_limits["resources"]["cpu"]["boundary"]
+                cont_guarded = cont_info["guard"]
+                cont_current_cpu = cont_info["resources"]["cpu"]["current"]
+                cont_min_cpu = cont_info["resources"]["cpu"]["min"]
+                cont_restricted_cpu = cont_min_cpu + 2 * cont_cpu_boundary
+                if cont_guarded or cont_current_cpu != cont_restricted_cpu:
+                    log_warning(
+                        "Container {0} is supposed to be restricted but it is not, restricting again".format(cont_name),
+                        self.debug)
+                    self.restrict_container(cont_info, cont_cpu_boundary)
+
+    def restrict_user(self, user):
+        user["accounting"]["restricted"] = True
+        # Set containers to unguarded and generate petitions to lower the resources for containers
+        apps = user["applications"]
+        for app_name in apps:
+            app_info = self.couchdb_handler.get_structure(app_name)
+            for cont_name in app_info["containers"]:
+                cont_info = self.couchdb_handler.get_structure(cont_name)
+                cont_limits = self.couchdb_handler.get_limits({"name": cont_name})  # TODO This is pretty shoddy I know
+                cont_cpu_boundary = cont_limits["resources"]["cpu"]["boundary"]
+                self.restrict_container(cont_info, cont_cpu_boundary)
+
+    def raise_restriction(self, user):
+        user["accounting"]["restricted"] = False
+        # Set containers to guarded so the Guardian picks up the scaling again (cold start)
+        apps = user["applications"]
+        for app_name in apps:
+            app_info = self.couchdb_handler.get_structure(app_name)
+            for cont_name in app_info["containers"]:
+                cont_info = self.couchdb_handler.get_structure(cont_name)
+                cont_info["guard"] = True
+                self.couchdb_handler.update_structure(cont_info)
+
+    def check_credit(self, user):
+        user_name = user["name"]
+        user_restricted = user["accounting"]["restricted"]
+        user_credit = user["accounting"]["credit"]
+        if user_restricted and user_credit <= 0:
+            log_warning("User {0} is restricted and not have enough credit, maintain restriction".format(user_name),
+                        self.debug)
+            self.check_restriction(user)
+        elif not user_restricted and user_credit < 0:
+            log_warning("User {0} is not restricted but does not have enough credit, restrict".format(user_name),
+                        self.debug)
+            self.restrict_user(user)
+        elif user_restricted and user_credit > 0:
+            log_warning("User {0} is restricted but has enough credit, raise restriction".format(user_name),
+                        self.debug)
+            self.raise_restriction(user)
+        elif not user_restricted and user_credit >= 0:
+            log_warning("User {0} is not restricted and has enough credit".format(user_name), self.debug)
+
+    def manage_thread(self, users):
+        users_wallets = self.get_users_wallets()
+
+        for u in users:
+            log_info("Processing User {0}".format(u["name"]), self.debug)
+
+            # Refresh the info regarding credit and coins
+            if u["name"] not in users_wallets:
+                log_warning("User {0} does not have a registered wallet", self.debug)
+                self.couchdb_handler.update_user(u)
+                continue
+
+            log_info("User {0} has {1} GRC".format(u["name"], users_wallets[u["name"]]), self.debug)
+            self.compute_credit_cpu(u, users_wallets)
+
+            # Refresh the information
+            self.compute_consumed_cpu(u)
+
+            # Rebase the counters
+            self.rebase_counters(u)
+
+            # Check that every user has enough credit
+            self.check_credit(u)
+
+            # Retrieve the user again in case it has been changed by the Refeder, merge the changed info and update it
+            u_current = self.couchdb_handler.get_user(u["name"])
+            u_current["accounting"]["pending"] = u["accounting"]["pending"]
+            u_current["accounting"]["coins"] = u["accounting"]["coins"]
+            u_current["accounting"]["restricted"] = u["accounting"]["restricted"]
+            u_current["accounting"]["credit"] = u["accounting"]["credit"]
+            self.couchdb_handler.update_user(u_current)
+
+            log_info("Updated User {0} (cpu)".format(u["name"]), self.debug)
+
+    def compute_credit_cpu(self, user, users_credits):
+        user_name = user["name"]
+        if user_name not in users_credits:
+            log_warning("User {0} does not have a wallet".format(user_name), self.debug)
+        else:
+            user["accounting"]["credit"] = self.coins_credit_ratio * users_credits[user_name]
+            user["accounting"]["coins"] = users_credits[user_name]
+
+    def compute_consumed_cpu(self, user):
+        billed_type = user["accounting"]["billing_type"]
+        if billed_type == "used":
+            cpu_consumed = user["cpu"]["used"]
+            # log_warning("Refeeder says {0}".format(cpu_consumed * self.polling_frequency), self.debug)
+            # # TESTING NEW CPU CONSUMPTION ACCOUNTABILITY
+            # summatory = 0
+            # apps = user["applications"]
+            # for app_name in apps:
+            #     app_info = self.couchdb_handler.get_structure(app_name)
+            #     for cont_name in app_info["containers"]:
+            #         cont_info = self.couchdb_handler.get_structure(cont_name)
+            #         start = int(time.time() - self.polling_frequency - 10)
+            #         end = int(time.time() - 10)
+            #         subquery = [dict(aggregator='zimsum', metric="proc.cpu.user", tags={"host": cont_info["name"]}, downsample="5s-avg")]
+            #         query = dict(start=start, end=end, queries=subquery)
+            #         cont_cpu_metrics = self.opentsdb_handler.get_points(query)[0]["dps"]
+            #         print(cont_cpu_metrics)
+            #         summatory = 0
+            #         points = list(cont_cpu_metrics.items())
+            #         if points:
+            #             # Perform the integration through trapezoidal steps
+            #             previous_time = int(points[0][0])
+            #             previous_value = points[0][1]
+            #             sum = previous_value
+            #             for point in points[1:]:
+            #                 t = int(point[0])
+            #                 value = point[1]
+            #                 diff_time = t - previous_time
+            #                 added_value = value + previous_value
+            #                 summatory += (added_value / 2) * diff_time
+            #                 sum += value
+            #                 previous_time = t
+            #                 previous_value = value
+            #         first_time=int(points[0][0])
+            #         last_time=int(points[-1][0])
+            #         diff_time=self.polling_frequency-(last_time-first_time)
+            #         log_warning("Residual time is {0}".format(diff_time), self.debug)
+            #         if diff_time:
+            #             summatory += points[0][1] * diff_time/2
+            #             summatory += points[-1][1] * diff_time/2
+            #         log_warning("Integral says says {0}".format(int(summatory)), self.debug)
+            #         #log_warning("Diff of  {0}".format(round(summatory / (cpu_consumed * self.polling_frequency),3)), self.debug)
+            #         log_warning("average says {0}".format(int(self.polling_frequency * sum / len(points))), self.debug)
+            # cpu_consumed = summatory
+            # ###########
+        elif billed_type == "current":
+            cpu_consumed = user["cpu"]["current"]
+        else:
+            cpu_consumed = user["cpu"]["used"]
+        cpu_consumed /= 100  # Convert shares to vcores
+        if cpu_consumed > self.threshold:
+            user["accounting"]["pending"] += cpu_consumed * self.polling_frequency # If the summatory is used above, remove the multiplication
+            #user["accounting"]["pending"] = round(user["accounting"]["pending"], 2)
+
+    def manage(self, ):
+        myConfig = MyConfig(CONFIG_DEFAULT_VALUES)
+        logging.basicConfig(filename=SERVICE_NAME + '.log', level=logging.INFO)
+        i = 0
+        while True:
+            t0 = start_epoch(self.debug)
+
+            # Get service info
+            service = get_service(self.couchdb_handler, SERVICE_NAME)
+
+            # Heartbeat
+            beat(self.couchdb_handler, SERVICE_NAME)
+
+            # CONFIG
+            myConfig.set_config(service["config"])
+            self.debug = myConfig.get_value("DEBUG")
+            debug = self.debug
+            self.polling_frequency = myConfig.get_value("POLLING_FREQUENCY")
+            self.grc_user = myConfig.get_value("GRIDCOIN_RPC_USER")
+            self.grc_pass = myConfig.get_value("GRIDCOIN_RPC_PASS")
+            self.grc_ip = myConfig.get_value("GRIDCOIN_RPC_IP")
+            self.grc_port = myConfig.get_value("GRIDCOIN_RPC_PORT")
+            self.coins_credit_ratio = myConfig.get_value("COINS_TO_CREDIT_RATIO")
+            self.min_coin_movement = myConfig.get_value("MIN_COIN_MOVEMENT")
+            SERVICE_IS_ACTIVATED = myConfig.get_value("ACTIVE")
+            self.threshold = myConfig.get_value("THRESHOLD")
+
+
+            log_info("Config is as follows:", debug)
+            log_info(".............................................", debug)
+            log_info("Polling frequency -> {0}".format(self.polling_frequency), debug)
+            log_info("GRC user -> {0}".format(self.grc_user), debug)
+            log_info("GRC password -> {0}".format(self.grc_pass), debug)
+            log_info("GRC ip -> {0}".format(self.grc_ip), debug)
+            log_info("GRC port -> {0}".format(self.grc_port), debug)
+            log_info("COINS to CREDIT ratio (vcore-s for 1 GRC) -> {0}".format(self.coins_credit_ratio), debug)
+            log_info("MIN GRC movement of (in GRC) -> {0}".format(self.min_coin_movement), debug)
+            log_info(".............................................", debug)
+
+            self.config_grc_connection()
+            if not self.test_grc_connection():
+                log_error("GRC connection failed", self.debug)
+
+            thread = None
+            if SERVICE_IS_ACTIVATED:
+                users = self.couchdb_handler.get_users()
+                users = [u for u in users if u["accounting"]["active"]]
+                if not users:
+                    log_info("No users to process", debug)
+                    time.sleep(self.polling_frequency)
+                    end_epoch(self.debug, self.polling_frequency, t0)
+                    continue
+                else:
+                    thread = Thread(target=self.manage_thread, args=(users,))
+                    thread.start()
+            else:
+                log_warning("CreditManager is not activated", debug)
+            t1 = time.time()
+            log_info("Time for processing was {0}".format(t1 - t0), debug)
+            time.sleep(self.polling_frequency - (t1 - t0))
+
+            wait_operation_thread(thread, debug)
+            log_info("Credit management processed", debug)
+
+            i += 1
+            if i == self.polling_frequency:
+                i = 0
+                log_warning("DECOUPLING FREQUENCY", debug)
+                time.sleep(1)
+
+            end_epoch(self.debug, self.polling_frequency, t0)
+
+def main():
+    try:
+        checker = CreditManager()
+        checker.manage()
+    except Exception as e:
+        log_error("{0} {1}".format(str(e), str(traceback.format_exc())), debug=True)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/other/open_chrome_socks.sh
+++
@@ -1,4 +0,0 @@
-tmux new -d -s 'SOCKS_SERVER1'  ssh -N -D 9090 jonatan@server1 -v
-tmux new -d -s 'CHROME_SOCKS' -- /usr/bin/google-chrome --user-data-dir="$HOME/proxy-profile" --proxy-server="socks5://localhost:9090"
-tmux ls
-
